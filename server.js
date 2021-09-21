9b34bb7628cc44b106c8655ef673779b83a17812
amalmohann <amal.mohan@storilabs.com>
2021-09-21 21:15:48 +0530

    mongodb integrated. Chat application completed.

--- server.js
+++ server.js
@@ -2,29 +2,50 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname))
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-var messages = [
-    {name:'Amal',message:"Hola!"},
-    {name:'Akhil',message:"Hola! Amal"}
-];
+var dbUrl = "mongodb://localhost/ChatApp";
+mongoose.connect(dbUrl, { useNewUrlParser: true});
+mongoose.connection
+    .once('open',()=>{console.log('MonGod is now Available for your prayers!');})
+    .on('error',(err)=>{console.log(console.log("err -> ",err," <- err"));})
 
-app.get('/messages',(req,res)=>{
-    res.send(messages);
+var Message = mongoose.model('Message', new mongoose.Schema({
+    name: String,
+    message: String
+}))
+
+
+app.get('/messages', (req, res) => {
+    Message.find({},(err,messages)=>{
+        res.send(messages);
+    })
 });
 
-app.post('/messages',(req,res)=>{
-    messages.push(JSON.parse(JSON.stringify(req.body)));
-    io.emit('message',  req.body);
-    res.sendStatus(200);
+app.post('/messages', (req, res) => {
+    var message = new Message(req.body);
+    message.save((err) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+        else {
+            io.emit('message', req.body);
+            res.sendStatus(200);
+        }
+    })
+
 });
 
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('user connected');
 })
+
+
 
 var server = http.listen(3000, () => {
     console.log("Node server is up! Listening to ", server.address().port);
